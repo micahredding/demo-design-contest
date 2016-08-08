09d4c7b481ce650de70d6e363176c8b8b6210458
ErnestasKlibavicius <ernestas.klibavicius@gmail.com>
2016-08-08 17:23:37 -0400

    update windows

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -57,7 +57,7 @@ class SubmissionsController < ApplicationController
 
     respond_to do |format|
       if @submission.update(submission_params)
-        format.html { redirect_to @submission, notice: 'Submission was successfully updated.' }
+        format.html { redirect_to contest_submission_path(@contest, @submission), notice: 'Submission was successfully updated.' }
         format.json { render :show, status: :ok, location: @submission }
       else
         format.html { render :edit }
--- db/schema.rb
+++ db/schema.rb
@@ -12,31 +12,31 @@
 
 ActiveRecord::Schema.define(version: 20160808202937) do
 
-  create_table "contests", force: :cascade do |t|
+  create_table "contests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "description", limit: 65535
     t.boolean  "published"
     t.boolean  "approved"
     t.integer  "reward"
     t.string   "category"
     t.string   "subcategory"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "submissions", force: :cascade do |t|
+  create_table "submissions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "description", limit: 65535
     t.boolean  "published"
     t.integer  "contest_id"
     t.integer  "rating"
     t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["contest_id"], name: "index_submissions_on_contest_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["contest_id"], name: "index_submissions_on_contest_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -49,8 +49,9 @@ ActiveRecord::Schema.define(version: 20160808202937) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "submissions", "contests"
 end
